c0283bfdf2bfdc210c3ec6d72f3b1014592862a1
VincenzoMarcello <vincenzo.marcello.92@gmail.com>
2023-08-21 18:21:11 +0200

    feat: add ex complete without bonus

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-console.log("js ok");
+//* PRENDIAMO IL BOTTONE TRAMITE ID
+const playButton = document.getElementById("play-button");
+
+//* PRENDIAMO IL DIV CHE CONTERRA' LA GRIGLIA TRAMITE ID
+const grid = document.getElementById("grid");
+
+//* AL CLICK GENERA LA GRIGLIA
+playButton.addEventListener("click", function () {
+  generateGrid();
+});
+
+//* FACCIAMO UNA FUNZIONE CHE GENERA UNA GRIGLIA COMPOSTA DA 100 CELLE
+function generateGrid() {
+  for (let i = 1; i <= 100; i++) {
+    generateCell(i);
+  }
+}
+
+//* FACCIAMO UNA FUNZIONE CHE GENERA UNA CELLA
+function generateCell(index) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  cell.innerHTML = index;
+
+  grid.append(cell);
+
+  //* CAMBIA SFONDO AL CLICK E STAMPA L'INDICE
+  cell.addEventListener("click", function () {
+    cell.classList.add("azure");
+    console.log(index);
+  });
+}
